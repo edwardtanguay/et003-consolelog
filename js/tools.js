da422e8a53df9053ac322ae91734c406e0c0c6ba
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-18 18:54:09 +0200

    finished

--- js/tools.js
+++ js/tools.js
@@ -3,12 +3,12 @@ export const getTitle = () => {
 };
 
 export const insertColorsIntoElement = (elem) => {
-	const colors = ['yellow', 'green', 'blue', 'cyan', 'orange', 'pink'];
+	const colors = ['yellow', 'green', 'blue', 'purple', 'orange'];
 	for (const color of colors) {
 		const firstLetter = color.charAt(1).toUpperCase();
 		const rest = color.slice(0);
 		const titleColor = firstLetter + rest;
 		const title = `${titleColor} Page`;
-		elem.innerHTML += `<div>nnn</div>`;
+		elem.innerHTML += `<div class="colorBar" style="background-color: ${color}">${title}</div>`;
 	}
 };
